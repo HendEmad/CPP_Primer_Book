4ee4743cafaa715e79da87c2144965c558223701
Hend Emad <32018228@hti.edu.eg>
2023-12-06 12:26:41 +0200

    Add files via upload

---
+++ Chapter 1/Exercises/Exercise 1.11.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cin >> n1 >> n2;
+    int i = n1;
+    while (i <= n2)
+    {
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
